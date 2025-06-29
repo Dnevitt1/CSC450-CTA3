98c945adf32432ade1195ead5527a04e9be7ca35
Dnevitt1 <168892117+Dnevitt1@users.noreply.github.com>
2025-06-29 00:48:48 -0700

    Update main.cpp
    fixed pointers holding garbage values

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 int main() {
 
     int n1, n2, n3;
+
+    std::cout << "Enter 3 numbers separated by spaces: " << std::endl;
+    std::cin >> n1 >> n2 >> n3;
+
     int *pN1 = new int(n1);
     int *pN2 = new int(n2);
     int *pN3 = new int(n3);
-
-    std::cout << "Enter 3 numbers separated by spaces: " << std::endl;
-    std::cin >> *pN1 >> *pN2 >> *pN3;
 
     std::cout << "Addresses: " << pN1 << ", " << pN2 << ", " << pN3 << std::endl;
     std::cout << "Values: " << *pN1 << ", " << *pN2 << ", " << *pN3 << std::endl;
